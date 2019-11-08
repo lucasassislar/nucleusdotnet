58079019a9a9a55651f3fe2593adf90a1c4d2d51
Lucas Assis <lucasassis@outlook.com>
2019-11-08 05:20:53 -0300

    Web routing

---
+++ Nucleus.Gaming/Nucleus.Gaming/Web/RouteAttribute.cs
@@ -0,0 +1,20 @@
+﻿using SimpleHttpServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleus.Gaming.Web {
+    [AttributeUsage(AttributeTargets.Method)]
+    public class RouteAttribute : Attribute {
+        public Route Route { get; set; }
+
+        public RouteAttribute(string method, string urlRegex) {
+            Route = new Route();
+            Route.Method = method;
+            Route.UrlRegex = urlRegex;
+            Route.Name = urlRegex;
+        }
+    }
+}
---
+++ Nucleus.Gaming/Nucleus.Gaming/Web/RouteManagerAttribute.cs
@@ -0,0 +1,17 @@
+﻿using SimpleHttpServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleus.Gaming.Web {
+    [AttributeUsage(AttributeTargets.Class)]
+    public class RouteManagerAttribute : Attribute {
+        public string UrlRegex { get; set; }
+
+        public RouteManagerAttribute(string urlRegex) {
+            UrlRegex = urlRegex;
+        }
+    }
+}
