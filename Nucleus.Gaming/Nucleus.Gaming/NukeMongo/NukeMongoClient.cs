90f6a2fb9ab543a04facf1c0a1577e727705031e
Lucas Assis <lucasassis@outlook.com>
2020-12-23 05:13:47 -0300

    micro mongo implementation start

---
+++ Nucleus.Gaming/Nucleus.Gaming/NukeMongo/NukeMongoClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleus.NukeMongo {
+    public class NukeMongoClient {
+
+        public NukeMongoClient() {
+            Initialize();
+        }
+
+        private void Initialize() {
+            // get our save folder
+            string startFolder = AssemblyUtil.GetStartFolder();
+
+        }
+    }
+}
