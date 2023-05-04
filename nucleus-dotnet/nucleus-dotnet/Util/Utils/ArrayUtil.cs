8337713ad8ae7d5c6af965426459a5ce526164a2
Lucas Assis <lucasassis@outlook.com>
2023-05-03 21:17:08 -0300

    better ui

--- nucleus-dotnet/nucleus-dotnet/Util/Utils/ArrayUtil.cs
+++ nucleus-dotnet/nucleus-dotnet/Util/Utils/ArrayUtil.cs
@@ -6,6 +6,21 @@ using System.Threading.Tasks;
 
 namespace Nucleus {
     public static class ArrayUtil {
+        public static bool IsContentsEqual<T>(T[] a, T[] b) {
+            if (a.Length != b.Length) {
+                return false;
+            }
+
+            for (int i = 0; i < a.Length; i++) {
+                T aa = a[i];
+                T bb = b[i];
+                if (!aa.Equals(bb)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
         public static T[] Join<T>(T[] a, T[] b) {
             T[] final = new T[a.Length + b.Length];
 
